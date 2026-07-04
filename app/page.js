4643f9b51bd91137cba205b25a0273dd4816d47a
AlikeDev <180838563+AlikeDev@users.noreply.github.com>
2026-07-04 12:53:55 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,1 @@
-export default function Home(){return <main style={{fontFamily:"Arial",padding:40,textAlign:"center"}}><h1>My Portfolio Deployment1</h1><p>Welcome!</p></main>}
+export default function Home(){return <main style={{fontFamily:"Arial",padding:40,textAlign:"center"}}><h1>My Portfolio Deployment2</h1><p>Welcome!</p></main>}
